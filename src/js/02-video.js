0c856a4b46256881f7a27ebdccfcbe9eb080fd22
atamaiNia <bimatideka@gmail.com>
2022-08-06 17:44:18 +0300

    task-2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+function onPlayVideo(time) {
+  //   console.log(time);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}
+
+player.on('timeupdate', throttle(onPlayVideo, 1000));
+
+const savedCurrentTime = localStorage.getItem('videoplayer-current-time');
+const parsedsavedCurrentTime = JSON.parse(savedCurrentTime);
+const { seconds } = parsedsavedCurrentTime;
+// console.log(seconds);
+
+player
+  .setCurrentTime(seconds)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
